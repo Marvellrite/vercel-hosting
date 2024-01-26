7ed3328739bd89ba5e9298718f6f3878dd3c4c11
Marvel <melodyozuru087@gmail.com>
2024-01-26 04:15:02 +0100

    Adjust the app.js file

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
+require("dotenv").config();
+
 const express = require("express");
 
 const app = express();
 
 app.use(express.static("public"));
 
-app.listen(5000, ()=>{
-    console.log("Listening at port 5000");
+const PORT = process.env.PORT;
+
+app.listen(PORT, ()=>{
+    console.log(`Listening at port ${PORT}`);
 });
 
 module.exports = app;
