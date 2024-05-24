a9c57e05adf1417e414fbf90dc073d5adc76ba7a
Marvel <melodyozuru087@gmail.com>
2024-05-24 17:05:59 +0100

    Final Changes

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const express = require("express");
 
 const app = express();
 
-// app.use(express.static("public"));
-
 app.get("/", (req, res)=>{
     res.status(200).send("<h1>Hello this is Marvel Showing how Vercel Hosting is done</h1><br><a href='/about.html'>About Page</a>")
 })
@@ -19,4 +17,3 @@ app.listen(PORT, ()=>{
     console.log(`Listening at port ${PORT}`);
 });
 
-module.exports = app;
