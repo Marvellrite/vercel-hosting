3c644fafdb3abf75c6fc74f747a9d0fcc76c7140
Marvel <melodyozuru087@gmail.com>
2024-07-12 21:16:28 +0100

    Removed some variables from app.js

--- app.js
+++ app.js
@@ -9,12 +9,10 @@ const envVariable = process.env.Variable;
 
 
 app.get("/", (req, res)=>{
-    const response = `<h1>Hello this is Ozuru Dev Showing how Vercel Hosting is done</h1><br><a href='/about.html'>About Page</a>`;
-    res.status(200).send(response);
+    res.status(200).send(`<h1>Hello this is Ozuru Dev Showing how Vercel Hosting is done</h1><br><a href='/about.html'>About Page</a>`);
 })
 app.get("/about.html", (req, res)=>{
-    const response = `<h1>Welcome to the about page</h1>. <br> <h3>The environment Variable is: ${envVariable}</h3> <br><a href='/home.html'>Home page</a>`;
-    res.status(200).send(response)
+    res.status(200).send(`<h1>Welcome to the about page</h1>. <br> <h3>The environment Variable is: ${envVariable}</h3> <br><a href='/home.html'>Home page</a>`)
 })
 
 
