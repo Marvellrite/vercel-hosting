e3dd2e17f3020e4e7242a83e79e8f60f785e0611
Marvel <melodyozuru087@gmail.com>
2024-07-12 08:21:47 +0200

    Add modifications to the app.js file

--- app.js
+++ app.js
@@ -4,15 +4,19 @@ const express = require("express");
 
 const app = express();
 
+const PORT = process.env.PORT;
+const envVariable = process.env.Variable;
+
+
 app.get("/", (req, res)=>{
     const response = `<h1>Hello this is Ozuru Dev Showing how Vercel Hosting is done</h1><br><a href='/about.html'>About Page</a>`;
     res.status(200).send(response);
 })
 app.get("/about.html", (req, res)=>{
-    res.status(200).send("<h1>Welcome to the about page</h1>")
+    const response = `<h1>Welcome to the about page</h1>. <br> <h3>The environment Variable is: ${envVariable}</h3> <br><a href='/home.html'>Home page</a>`;
+    res.status(200).send(response)
 })
 
-const PORT = process.env.PORT;
 
 app.listen(PORT, ()=>{
     console.log(`Listening at port ${PORT}`);
