2741bbaf8a164fa8addb3e3c4da770f5b8bbea29
Marvel <melodyozuru087@gmail.com>
2024-08-04 05:23:30 +0100

    Make adjustment for the channel environment variable

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const express = require("express");
 
 const app = express();
 
-const PORT = 3000;
+const PORT = 5000;
 
 app.get("/", (req, res)=>{
-    res.status(200).send(`<h1>Hello this is Ozuru Dev Showing how Vercel Hosting is done</h1><br><a href='/about.html'>About Page</a>`);
+    res.status(200).send(`<h1>Hello this is ${process.env.CHANNEL} Showing how Vercel Hosting is done</h1><br><a href='/about.html'>Go to About Page</a>`);
 })
 app.get("/about.html", (req, res)=>{
-    res.status(200).send(`<h1>Welcome to the about page</h1>. <br> <h3>The environment Variable is: ${envVariable}</h3> <br><a href='/home.html'>Home page</a>`)
+    res.status(200).send(`<h1>Welcome to the about page</h1>. <br> <br><a href='/'>Go to Home page</a>`)
 })
 
 
